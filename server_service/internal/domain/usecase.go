0f9e8d26cb3c112a797870b6276c9871971a843e
lits-06 <minhanh0160@gmail.com>
2025-08-28 17:30:40 +0700

    refactor: request

--- server_service/internal/domain/usecase.go
+++ server_service/internal/domain/usecase.go
@@ -17,29 +17,9 @@ type UseCase interface {
 	ExportServersToExcel(ctx context.Context, req QueryServerRequest) error
 }
 
-type CreateServerRequest struct {
-	ID     string              `json:"id" validate:"required"`
-	Name   string              `json:"name" validate:"required"`
-	IPv4   string              `json:"ipv4" validate:"required,ipv4"`
-	Status entity.ServerStatus `json:"status" validate:"omitempty,oneof=ON OFF"`
-}
-
-type QueryServerRequest struct {
-	Filter     ServerFilter     `json:"filter"`
-	Pagination ServerPagination `json:"pagination"`
-	Sort       ServerSort       `json:"sort"`
-}
-
 type QueryServerResponse struct {
 	Servers *[]entity.Server `json:"servers"`
 	Total   int              `json:"total"`
-}
-
-type UpdateServerRequest struct {
-	ID     string              `json:"id"`
-	Name   string              `json:"name,omitempty" validate:"omitempty"`
-	IPv4   string              `json:"ipv4,omitempty" validate:"omitempty,ipv4"`
-	Status entity.ServerStatus `json:"status,omitempty" validate:"omitempty,oneof=ON OFF"`
 }
 
 type ImportResponse struct {
