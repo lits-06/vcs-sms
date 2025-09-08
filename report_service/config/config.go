e9fcb0672bcd7e8e748e5041a65094cff8322d2b
lits-06 <minhanh0160@gmail.com>
2025-09-08 23:40:49 +0700

    feat: report service config

--- report_service/config/config.go
+++ report_service/config/config.go
@@ -1,1 +1,99 @@
 package config
+
+import (
+	"github.com/lits-06/vcs-sms/pkg/logger"
+	"github.com/lits-06/vcs-sms/pkg/tracing"
+)
+
+var configPath string
+
+func init() {
+	flag.StringVar(&configPath, "config", "", "Report microservice config path")
+}
+
+type Config struct {
+	ServiceName      string              `mapstructure:"serviceName"`
+	Logger           *logger.Config      `mapstructure:"logger"`
+	KafkaTopics      KafkaTopics         `mapstructure:"kafkaTopics"`
+	GRPC             GRPC                `mapstructure:"grpc"`
+	Postgresql       *postgres.Config    `mapstructure:"postgres"`
+	Kafka            *kafka.Config `mapstructure:"kafka"`
+	Redis            *redis.Config       `mapstructure:"redis"`
+	Probes           probes.Config       `mapstructure:"probes"`
+	ServiceSettings  ServiceSettings     `mapstructure:"serviceSettings"`
+	Jaeger           *tracing.Config     `mapstructure:"jaeger"`
+
+	Smtp			Smtp `mapstructure:"smtp"`
+}
+
+type Smtp struct {
+	Host       string   `mapstructure:"host" validate:"required"`
+	Port       int      `mapstructure:"port" validate:"required,min=1,max=65535"`
+	Username   string   `mapstructure:"username" validate:"required"`
+	Password   string   `mapstructure:"password" validate:"required"`
+	From       string   `mapstructure:"from" validate:"required,email"`
+	To         []string `mapstructure:"to" validate:"required,dive,email"`
+}
+
+func InitConfig() (*Config, error) {
+	if configPath == "" {
+		configPathFromEnv := os.Getenv(constants.ConfigPath)
+		if configPathFromEnv != "" {
+			configPath = configPathFromEnv
+		} else {
+			getwd, err := os.Getwd()
+			if err != nil {
+				return nil, errors.Wrap(err, "os.Getwd")
+			}
+			configPath = fmt.Sprintf("%s/server_service/config/config.yaml", getwd)
+		}
+	}
+
+	cfg := &Config{}
+
+	viper.SetConfigType(constants.Yaml)
+	viper.SetConfigFile(configPath)
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, errors.Wrap(err, "viper.ReadInConfig")
+	}
+
+	if err := viper.Unmarshal(cfg); err != nil {
+		return nil, errors.Wrap(err, "viper.Unmarshal")
+	}
+
+	grpcPort := os.Getenv(constants.GrpcPort)
+	if grpcPort != "" {
+		cfg.GRPC.Port = grpcPort
+	}
+	postgresHost := os.Getenv(constants.PostgresqlHost)
+	if postgresHost != "" {
+		cfg.Postgresql.Host = postgresHost
+	}
+	postgresPort := os.Getenv(constants.PostgresqlPort)
+	if postgresPort != "" {
+		cfg.Postgresql.Port = postgresPort
+	}
+	redisAddr := os.Getenv(constants.RedisAddr)
+	if redisAddr != "" {
+		cfg.Redis.Addr = redisAddr
+	}
+	//jaegerAddr := os.Getenv("JAEGER_HOST")
+	//if jaegerAddr != "" {
+	//	cfg.Jaeger.HostPort = jaegerAddr
+	//}
+	//kafkaBrokers := os.Getenv("KAFKA_BROKERS")
+	//if kafkaBrokers != "" {
+	//	cfg.Kafka.Brokers = []string{"host.docker.internal:9092"}
+	//}
+	kafkaBrokers := os.Getenv(constants.KafkaBrokers)
+	if kafkaBrokers != "" {
+		cfg.Kafka.Brokers = []string{kafkaBrokers}
+	}
+	jaegerAddr := os.Getenv(constants.JaegerHostPort)
+	if jaegerAddr != "" {
+		cfg.Jaeger.HostPort = jaegerAddr
+	}
+
+	return cfg, nil
+}
