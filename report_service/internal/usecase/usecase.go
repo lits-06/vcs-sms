eb473a83f9f6c1e47054ccafd33e38444395d4e3
lits-06 <minhanh0160@gmail.com>
2025-09-08 23:40:38 +0700

    feat: report service uc

--- report_service/internal/usecase/usecase.go
+++ report_service/internal/usecase/usecase.go
@@ -8,67 +8,80 @@ import (
 	"path/filepath"
 	"time"
 
+	"github.com/opentracing/opentracing-go"
+	"github.com/lits-06/vcs-sms/pkg/tracing"
 	"github.com/lits-06/vcs-sms/pkg/utils"
+	"github.com/lits-06/vcs-sms/report_service/config"
 	"github.com/lits-06/vcs-sms/report_service/internal/domain"
 	"gopkg.in/gomail.v2"
 )
 
 type reportUseCase struct {
 	repo domain.Repository
+	cfg  *config.Config
 }
 
-func NewReportUseCase(repo domain.Repository) domain.UseCase {
-	return &reportUseCase{repo: repo}
+func NewReportUseCase(repo domain.Repository, cfg *config.Config) domain.UseCase {
+	return &reportUseCase{repo: repo, cfg: cfg}
 }
 
 func (uc *reportUseCase) ReportStats(ctx context.Context, req *domain.UptimeRequest) error {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "reportUseCase.ReportStats")
+	defer span.Finish()
+
 	stats, err := uc.repo.GetUptimeStats(ctx, req)
 	if err != nil {
-		return err
+		return tracing.TraceWithErr(span, fmt.Errorf("failed to get uptime stats: %w", err))
 	}
 
-	return uc.sendUptimeReport(stats)
+	return uc.sendUptimeReport(ctx, stats)
 }
 
-func (uc *reportUseCase) sendUptimeReport(stats *domain.UptimeStats) error {
-	htmlBody, err := uc.generateUptimeReportHTML(stats)
+func (uc *reportUseCase) sendUptimeReport(ctx context.Context, stats *domain.UptimeStats) error {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "reportUseCase.sendUptimeReport")
+	defer span.Finish()
+
+	htmlBody, err := uc.generateUptimeReportHTML(ctx, stats)
 
 	if err != nil {
-		return err
+		return tracing.TraceWithErr(span, err)
 	}
 
 	m := gomail.NewMessage()
-	m.SetHeader("From", uc.config.From)
-	m.SetHeader("To", uc.config.AdminEmail...)
+	m.SetHeader("From", uc.cfg.Smtp.From)
+	m.SetHeader("To", uc.cfg.Smtp.To...)
 	m.SetHeader("Subject", fmt.Sprintf("Server Uptime Report - %s to %s",
 		stats.StartDate.Format(time.DateTime),
 		stats.EndDate.Format(time.DateTime)))
 	m.SetBody("text/html", htmlBody)
 
-	d := gomail.NewDialer(uc.config.Host, uc.config.Port, uc.config.Username, uc.config.Password)
+	d := gomail.NewDialer(uc.cfg.Smtp.Host, uc.cfg.Smtp.Port, uc.cfg.Smtp.Username, uc.cfg.Smtp.Password)
 	if err := d.DialAndSend(m); err != nil {
-		return err
+		return tracing.TraceWithErr(span, fmt.Errorf("failed to send email: %w", err))
 	}
 
 	return nil
 }
 
-func (uc *reportUseCase) generateUptimeReportHTML(stats *domain.UptimeStats) (string, error) {
+func (uc *reportUseCase) generateUptimeReportHTML(ctx context.Context, stats *domain.UptimeStats) (string, error) {
+	span, ctx := opentracing.StartSpanFromContext(ctx, "reportUseCase.generateUptimeReportHTML")
+	defer span.Finish()
+
 	projectRoot, err := utils.FindProjectRoot()
 	if err != nil {
-		return "", err
+		return "", tracing.TraceWithErr(span, fmt.Errorf("failed to find project root: %w", err))
 	}
 
 	templatePath := filepath.Join(projectRoot, "report_service", "internal", "templates", "uptime_report.html")
 
 	t, err := template.ParseFiles(templatePath)
 	if err != nil {
-		return "", err
+		return "", tracing.TraceWithErr(span, fmt.Errorf("failed to parse template: %w", err))
 	}
 
 	var buf bytes.Buffer
 	if err := t.Execute(&buf, stats); err != nil {
-		return "", err
+		return "", tracing.TraceWithErr(span, fmt.Errorf("failed to execute template: %w", err))
 	}
 
 	return buf.String(), nil
